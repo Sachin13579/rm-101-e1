bbdb2de0ebb8ec4a888537cf4aa7198c9cbd525a
Sachin13579 <97518055+Sachin13579@users.noreply.github.com>
2022-06-08 20:53:44 +0530

    css remaining

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./addTask.module.css";
 
-const AddTask = () => {
+const AddTask = ({ addTask }) => {
   // NOTE: do not delete `data-testid` key value pair
+  const [task, seTask] = useState("");
+  const handleClick = () => {
+    addTask(task);
+    seTask("");
+  };
+
   return (
     <div className={styles.todoForm}>
-      <input data-testid="add-task-input" type="text" />
-      <button data-testid="add-task-button"></button>
+      <input data-testid="add-task-input" type="text" value={task} onChange={({ target }) => seTask(target.value)}/>
+      <button data-testid="add-task-button" onClick={handleClick}>+</button>
     </div>
   );
 };
